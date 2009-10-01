06f94e19bc296cfddd56b7e2b99fbb279c0204ae
Dima <megaspambox@mail.ru>
2009-10-02 02:38:48 +0400

    задание 1.2 выполнено

--- lab1/src/lab1/Main.java
+++ lab1/src/lab1/Main.java
@@ -14,11 +14,21 @@ package lab1;
  */
 public class Main {
 
+    // 1.2
+    public String var1 = "Some";
+    protected String var2 = "super";
+    private String var3 = "secret";
+    String var4 = "or friendly";
+
     public Main() {
         // 1.1
         java.util.ArrayList al = new java.util.ArrayList();
         al.add(new Integer(1));
         System.out.println("ArrayList size is " + al.size() + " element(s)");
+    }
+
+    private void privateMethod() {
+        
     }
     
     public static void main(String[] args) {
