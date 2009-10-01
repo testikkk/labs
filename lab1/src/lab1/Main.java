6039886d41ef364556039c8f0f9387a2ecf077d2
Dima <megaspambox@mail.ru>
2009-10-02 02:23:55 +0400

    lab1 каркас

---
+++ lab1/src/lab1/Main.java
@@ -0,0 +1,21 @@
+/**
+ * Первая лабораторная работа
+ *
+ * @author ya
+ * @version 1.0
+ * @company VSTU
+ */
+
+package lab1;
+
+/**
+ *
+ * @author root
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+
+}
